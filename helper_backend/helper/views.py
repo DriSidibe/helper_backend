9bfc22b6093eaa3b07cb2615e8a3fb522001265c
DriSidibe <you@example.com>
2023-08-23 20:02:31 +0000

    commit

--- helper_backend/helper/views.py
+++ helper_backend/helper/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 
 def home(request):
@@ -12,14 +12,35 @@ def home(request):
 
 @csrf_exempt
 def signup(request):
-    resp = request.POST
-    print(resp)
-    user = User.objects.create_user(
-        username=resp["username"],
-        email=resp["email"],
-        password=resp["password"],
-    )
-    user.last_name = resp["lastname"]
-    user.first_name = resp["firstname"]
-    user.save()
-    return JsonResponse({"message": "Signup page"})
+    response_good = HttpResponse(0)
+    response_bad = HttpResponse(1)
+    response_very_bad = HttpResponse(-1)
+    try:
+        resp = request.POST
+        try:
+            user1 = User.objects.get(username=resp["username"])
+        except:
+            user1 = None
+            try:
+                user2 = User.objects.get(email=resp["email"])
+            except:
+                user2 = None
+        if (user1) or (user2):
+            return response_bad
+        else:
+            user = User.objects.create_user(
+                username=resp["username"],
+                email=resp["email"],
+                password=resp["password"],
+            )
+            user.last_name = resp["lastname"]
+            user.first_name = resp["firstname"]
+            user.save()
+            user = authenticate(username=resp["username"], password=resp["password"])
+            if user is not None:
+                return response_good
+            else:
+                return response_very_bad
+    except Exception as e:
+        print(e)
+        return response_very_bad
--- helper_backend/helper_backend/settings.py
+++ helper_backend/helper_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "helper",
 ]
 
@@ -48,6 +49,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "helper_backend.urls"
@@ -122,3 +125,12 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# cors settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
