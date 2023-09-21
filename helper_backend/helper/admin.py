f490db355bfd4effb9f469da4833a205e834cadf
DriSidibe <you@example.com>
2023-09-21 22:47:46 +0000

    update

--- helper_backend/helper/admin.py
+++ helper_backend/helper/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import CustomUser
-from helper_backend.tutorial.models import TutorialField, Chapiter
-from helper_backend.blog.models import BlogPost, PostContent
+from tutorial.models import TutorialField, Chapiter
+from blog.models import BlogPost, PostContent
 
 admin.site.register(CustomUser)
 admin.site.register(TutorialField)
